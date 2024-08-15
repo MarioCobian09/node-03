88fd071727497703218db81a9cae13103ee0fc3e
Mario Cobian <marvanis@hotmail.com>
2024-08-15 08:37:15 -0600

    Modificacion del gitignore

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('hola');
+import express from "express"
+import bodyParser from "body-parser"
+import admin from "firebase-admin"
+import estudianteRoutes from "./routes/estudiantes.js"
+import ServiceAccount from "./config/firebaseServiceAccount.json" with { type: 'json' }
