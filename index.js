c62d10f297175681ef872f44bd47a492e5230c2d
Mario Cobian <marvanis@hotmail.com>
2024-08-15 08:41:07 -0600

    Agregamos codigo a index

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import bodyParser from "body-parser"
 import admin from "firebase-admin"
 import estudianteRoutes from "./routes/estudiantes.js"
 import ServiceAccount from "./config/firebaseServiceAccount.json" with { type: 'json' }
+
+admin.initializeApp({
+    credential: ServiceAccount
+})
+
+const app = express()
+app.use(bodyParser.json())
+
+app.use('/api/estudiantes', estudianteRoutes)
+
+const PORT = process.env.PORT || 3010
+
+app.listen(PORT, () => {
+    console.log(`SERVIDOR CORRIENDO EN ${PORT}`);
+})
