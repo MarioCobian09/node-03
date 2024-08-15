f490551ab191b82ff98e740076378caf67de2a76
Mario Cobian <marvanis@hotmail.com>
2024-08-15 08:53:54 -0600

    Comenzando ruta de estudiantes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express"
 import bodyParser from "body-parser"
 import admin from "firebase-admin"
 import estudianteRoutes from "./routes/estudiantes.js"
-import ServiceAccount from "./config/firebaseServiceAccount.json" with { type: 'json' }
+import ServiceAccount from "./config/firebase/firebaseServiceAccount.json" with { type: 'json' }
 
 admin.initializeApp({
     credential: ServiceAccount
---
+++ routes/estudiantes.js
@@ -0,0 +1,5 @@
+import express from "express"
+import bcrypt from "bcryptjs"
+import admin from "firebase-admin"
+
+const router = express.Router()
